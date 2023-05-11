c47ec78808f2662b218dd235d23954f13789f448
iiacoban42 <i.p.iacoban@gmail.com>
2023-05-11 14:02:22 +0200

    Add django server setup

---
+++ backend/src/home/views.py
@@ -0,0 +1,11 @@
+"""API request handling. Map requests to the corresponding HTMLs."""
+from django.shortcuts import render
+
+
+
+def home(request):
+    """Render index.html page"""
+    ren = render(request, "home/index.html")
+    return ren
+
+
