0ed495c25a5a35b8c0c5dd4809e2880e6fac9af6
iiacoban42 <i.p.iacoban@gmail.com>
2023-05-11 14:03:22 +0200

    Add docker files fro django

---
+++ backend/scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# force the startup script to fail if one of the comamnds fails
+set -e
+
+# download the Django static content and clear eventual leftover files
+python manage.py collectstatic --noinput --clear
+
+# run a TCP socket
+uwsgi --socket :8000 --master --module pickmeup.wsgi --enable-threads --processes 1 --threads 2
