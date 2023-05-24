3a08d45f494ef9e12c23e99f259d32b4d7ecb48f
AaronvDiepen <aaronvdiepen@gmail.com>
2023-05-24 14:23:30 +0200

    Add default bot that can join and leave voice channels

---
+++ bot/client.js
@@ -0,0 +1,58 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    switch (interaction.commandName) {
+        case 'join':
+            await join(interaction)
+            break;
+        case 'leave':
+            await leave(interaction)
+            break;
+        default:
+            await interaction.reply("Unknown command")
+            break;
+    }
+});
+
+async function join(interaction) {
+    if (!interaction.member.voice.channel) {
+        await interaction.reply("You must be in a voice channel for me to join you!");
+        return;
+    }
+
+    const voiceChannel = interaction.member.voice.channel;
+    try {
+        const connection = await joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+        });
+        await interaction.reply("Let's recommend some music!");
+    } catch (error) {
+        console.error(`Failed to join the voice channel: ${error}`);
+        await interaction.reply('Failed to join the voice channel. Please try again later.');
+    }
+}
+
+async function leave(interaction) {
+    let channel = interaction.member.voice.channel
+    const connection = getVoiceConnection(channel.guild.id);
+    try {
+        connection.destroy();
+        await interaction.reply("Hate to see you leave but love to watch you go!")
+    } catch (error) {
+        console.error(`Failed to leave the voice channel: ${error}`);
+        await interaction.reply('Failed to leave the voice channel. Please try again later.');
+    }
+}
+
+client.login(process.env.TOKEN);
