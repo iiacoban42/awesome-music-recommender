339a0d923731374871468324f91faa1ba5eebb03
iiacoban42 <i.p.iacoban@gmail.com>
2023-05-25 19:42:38 +0200

    Fetch music url in discord bot

--- bot/musicRecommender.js
+++ bot/musicRecommender.js
@@ -2,12 +2,21 @@ require('json5/lib/register');
 const contexts = require('./contexts.json5');
 
 let context = null;
+let activity_name = ""
 
 async function askContext(textChannel, context, question) {
     const message = await textChannel.send(question);
     for (let emojiOption in context) {
         await message.react(emojiOption);
     }
+}
+
+async function fetchMusic(activity, context) {
+    await fetch(`http://127.0.0.1:8000/get-new-track/${activity}/${context}/`)
+    .then(response => {
+        return response.json()
+            }
+        ).catch(e => console.log(e));
 }
 
 module.exports = function(client, interaction) {
@@ -27,9 +36,16 @@ module.exports = function(client, interaction) {
 
         if (!Object.keys(context).includes("contexts")) { // Check if we are done specifying the context
             interaction.channel.send(`Context selected ${JSON.stringify(context)}`);
-            return;
-        }
+            let context_name = context.name
+            await fetchMusic(activity_name, context_name)
+            .then(url => {
+                interaction.channel.send(`Track URL ${url}`);
+            });
 
+            return;
+        }else{
+            activity_name = context.name
+        }
         context = context.contexts; // Narrow down the context further
         askContext(interaction.channel, context, 'Specify the context further:')
     });
