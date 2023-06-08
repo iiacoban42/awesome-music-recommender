8cf1bad13b40ad93ce0770621fc6b682987ac7e2
iiacoban42 <i.p.iacoban@gmail.com>
2023-06-08 14:47:03 +0200

    Query youtube for song and return url

--- backend/src/home/recommender.py
+++ backend/src/home/recommender.py
@@ -1,55 +1,23 @@
-import pandas as pd
-from surprise import Dataset, Reader, KNNBasic
-from surprise.model_selection import train_test_split
+from googleapiclient.discovery import build
+import os
 
-# Example list of data
-data_list = [
-    ['user1', 'item1', 4],
-    ['user1', 'item2', 5],
-    ['user2', 'item1', 3],
-    ['user2', 'item3', 2],
-    ['user3', 'item2', 1],
-    ['user3', 'item3', 4]
-]
+# YouTube Data API key
+youtube_api_key = os.environ.get('YOUTUBE_API_KEY', 'change_me')
 
-# Convert the list of data to a pandas DataFrame
-df = pd.DataFrame(data_list, columns=['user', 'item', 'rating'])
+def getYoutubeURL(song_title: str, artist_name: str) -> str:
 
-# Define the rating scale
-reader = Reader(rating_scale=(1, 5))
+    # Initialize the YouTube Data API
+    youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-# Load the dataset using the defined rating scale
-data = Dataset.load_from_df(df[['user', 'item', 'rating']], reader)
+    # Perform the search based on title and artist
+    search_query = f"{song_title} {artist_name}"
+    search_response = youtube.search().list(q=search_query, part='id', maxResults=1, type='video').execute()
 
-# Split the dataset into training and testing sets
-trainset, testset = train_test_split(data, test_size=0.2)
+    # Retrieve the video ID of the top search result
+    video_id = search_response['items'][0]['id']['videoId']
 
-# Create an instance of the collaborative filtering algorithm
-algo = KNNBasic()
+    # Generate the YouTube URL
+    youtube_url = f"https://www.youtube.com/watch?v={video_id}"
 
-# Train the algorithm on the training set
-algo.fit(trainset)
-
-# Specify the list of users for whom you want to generate recommendations
-user_ids = ['user1', 'user2', 'user3']
-
-# Generate recommendations for each user
-for user_id in user_ids:
-    # Get a list of all items in the dataset
-    all_items = trainset.all_items()
-
-    # Filter out the items that the user has already rated
-    rated_items = [item_id for (item_id, _) in trainset.ur[trainset.to_inner_uid(user_id)]]
-    items_to_rate = [item_id for item_id in all_items if item_id not in rated_items]
-
-    # Make predictions for the user-item pairs
-    predictions = algo.test([(trainset.to_inner_uid(user_id), item_id, 4.0) for item_id in items_to_rate])
-
-    # Sort the predictions by the estimated rating
-    top_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)
-
-    # Print the recommended items for the user
-    print(f"User: {user_id}")
-    for prediction in top_predictions[:5]:
-        print(f"Item: {prediction.iid}, Predicted Rating: {prediction.est}")
-    print()
+    # Print the YouTube URL
+    return youtube_url
