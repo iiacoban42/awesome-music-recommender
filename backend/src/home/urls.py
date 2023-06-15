a106ae923200fd51a97caf03f3c5e2744fd9c1fd
iiacoban42 <i.p.iacoban@gmail.com>
2023-06-15 11:32:15 +0200

    Add csrf exception

--- backend/src/home/urls.py
+++ backend/src/home/urls.py
@@ -1,5 +1,6 @@
 """API urls"""
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -7,7 +8,7 @@ urlpatterns = [
     path('', views.home, name='homepage'),
     path('get-new-track/<activity>/<context>/', views.get_new_track, name='get_new_track'),
     path('get-new-track/<activity>/', views.get_new_track, name='get_new_track'),
-    path('get-new-playlist/', views.get_new_playlist, name='get_new_playlist'),
+    path('get-new-playlist/', csrf_exempt(views.get_new_playlist), name='get_new_playlist'),
     path('get-yt-url/<track>/<artist>/', views.get_youtube_url, name='get_youtube_url'),
 
 ]
