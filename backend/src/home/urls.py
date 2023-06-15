e8991d23a84b05d66f3deaf64e31de9f7f96f216
iiacoban42 <i.p.iacoban@gmail.com>
2023-06-15 15:53:17 +0200

    Fix url bug

--- backend/src/home/urls.py
+++ backend/src/home/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('get-new-track/<activity>/', views.get_new_track, name='get_new_track'),
     path('get-new-playlist/', csrf_exempt(views.get_new_playlist), name='get_new_playlist'),
     path('get-dummy-playlist/', csrf_exempt(views.get_dummy_playlist), name='get_dummy_playlist'),
-    path('get-yt-url/<track>/<artist>/', views.get_youtube_url, name='get_youtube_url'),
+    path('get-yt-url/<track>/<artist>/', views.get_track_youtube_url, name='get_track_youtube_url'),
 
 ]
